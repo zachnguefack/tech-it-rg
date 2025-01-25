b2d2d134afd60b8e30faca37fc381508cc2778e0
zachnguefack <zacharienguefack@gmail.com>
2025-01-25 21:12:32 +0100

    change 8

--- Program.cs
+++ Program.cs
@@ -2,9 +2,14 @@ using Blazor.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
