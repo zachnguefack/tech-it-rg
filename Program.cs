a3cbd4a1379901e68a79a2f6e9c3092f99dd6229
zachnguefack <zacharienguefack@gmail.com>
2025-01-26 10:29:53 +0100

    change 9

--- Program.cs
+++ Program.cs
@@ -8,8 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
